86e2588741a5118cfa64577357cf9b17e1953392
Tejas <ptejas.patelia@gmail.com>
2021-06-17 20:19:03 +0530

    Initial Commit

---
+++ Web-Scrapping-Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Web-Scrapping-Quotes
+//
+//  Created by Tejas Patelia on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
